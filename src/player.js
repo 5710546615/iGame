6fe42cd2c8519fbb6efb9f7ce9b39944b0e46335
tongty <tongty_sg@hotmail.com>
2015-04-01 16:34:52 +0700

    ex

--- src/player.js
+++ src/player.js
@@ -2,15 +2,9 @@ var player = cc.Sprite.extend({
     ctor: function(x,y) {
         this._super();
         this.initWithFile( 'res/player.png' );
-         this.setAnchorPoint(0,0);
 
         this.x = x;
         this.y = y;
-
-        this.moveLeft = false;
-        this.moveRight = false;
-
-        // this.updateSpritePosition();
 
 
     },
@@ -18,8 +12,18 @@ var player = cc.Sprite.extend({
     update: function( dt ) {
     var pos = this.getPosition();
 
+    if(pos.x < screenWidth)
     this.setPosition( new cc.Point( pos.x, 0 ) );
 
     },
+    switchDirection: function() {
+        if ( this.direction == Ship.DIR.UP ) {
+        this.direction = Ship.DIR.RIGHT;
+        this.setRotation( 90 );
+        } else {
+        this.direction = Ship.DIR.UP;
+        this.setRotation( 0 );
+    }
+    }
 
 });
